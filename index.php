84b0c83cb46bcb4198f9ee3e44eda7c439aec416
ArtifyZe <65348009+ArtifyZe@users.noreply.github.com>
2020-11-12 18:02:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Testing Web</title>
+    </head>
+
+<body>
+    Welcome <?php echo $_GET["name"]; ?><br>
+    our email address is: <?php echo $_GET["email"]; ?>
+</body>
+
+</html>
